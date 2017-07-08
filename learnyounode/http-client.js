2098ae75d14031e3551ad77abef0befff132585f
Martin Cross <mncross@gmail.com>
2017-07-08 14:05:47 -0500

    build first http client

---
+++ learnyounode/http-client.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+const url = process.argv[2]
+
+http.get(url, (response) => {
+    response.setEncoding('utf8')
+    response.on('error', console.error)
+    response.on('data', console.log)
+})
