91de7c9d2cbb246e82890ba28d0ddd7631197538
Martin Cross <mncross@gmail.com>
2017-08-15 08:44:03 -0500

    add writeHead as recommended in solution

--- learnyounode/http-file-server.js
+++ learnyounode/http-file-server.js
@@ -5,6 +5,7 @@ const port = process.argv[2]
 const filePath = process.argv[3]
 
 const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'content-type': 'text/plain' })
     fs.createReadStream(filePath).pipe(response)
 })
 
